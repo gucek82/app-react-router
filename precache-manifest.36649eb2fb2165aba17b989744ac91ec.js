a3fa741b2e70611f9a0cf95f93d9ff85a918ef11
Krzysztof Sarna <krzysztof.sarna@gmail.com>
2019-10-30 10:39:03 +0100

    Updates

---
+++ precache-manifest.36649eb2fb2165aba17b989744ac91ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94a1ae922697e03d206de30192b74af",
+    "url": "git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/index.html"
+  },
+  {
+    "revision": "e2a5cdc46ab234287ff6",
+    "url": "git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/static/css/main.ca8ec31a.chunk.css"
+  },
+  {
+    "revision": "d08505d7967228b414b6",
+    "url": "git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/static/js/2.e8d83c46.chunk.js"
+  },
+  {
+    "revision": "e2a5cdc46ab234287ff6",
+    "url": "git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/static/js/main.2440b21f.chunk.js"
+  },
+  {
+    "revision": "cbe73d34c8c0d1758748",
+    "url": "git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/static/js/runtime-main.25d3a14f.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/static/media/header3.d9d36b65.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/precache-manifest.36649eb2fb2165aba17b989744ac91ec.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("git%20remote%20add%20origin%20https://github.com/gucek82/app-react-router.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
